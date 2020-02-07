8ca7674c392e65960d20604f11e8094930c147c2
DomTripodi93 <dtripodi93@gmail.com>
2020-02-07 17:46:42 -0500

    updated header routes, and basic routing

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -5,13 +5,23 @@ import { SigninComponent } from './register/signin/signin.component';
 import { SignoutComponent } from './register/signout/signout.component';
 import { AuthGuard } from './shared/guards/auth.guard';
 import { HomeComponent } from './shared/home/home.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { ScheduleComponent } from './employees/schedule/schedule.component';
+import { DepartmentComponent } from './department/department.component';
+import { ObjectiveComponent } from './department/objective/objective.component';
 
 const appRoutes: Routes = [
     {path: '', component: HomeComponent, pathMatch: 'full' },
     {path: 'register', component: RegisterComponent },
     {path: 'login', component: SigninComponent},
     {path: "", runGuardsAndResolvers: "always", canActivate: [AuthGuard], children: [
-        {path: 'logout', component: SignoutComponent}
+        {path: 'logout', component: SignoutComponent},
+        {path: 'employee', component: EmployeesComponent, children: [
+            {path:'{employeeId}', component: ScheduleComponent}
+        ]},
+        {path: 'schedule', component: ScheduleComponent},
+        {path: 'department', component: DepartmentComponent},
+        {path: 'objective', component: ObjectiveComponent}
     ]},
     {path: "**", redirectTo:"/", pathMatch: "full"}
 ];

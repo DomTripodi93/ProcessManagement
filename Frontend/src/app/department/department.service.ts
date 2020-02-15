15ebd65fea187bea7f6f83ce13746fafbade8f47
DomTripodi93 <dtripodi93@gmail.com>
2020-02-15 04:28:53 -0500

    abstracted http service for reusable http calls based on API configuration

--- Frontend/src/app/department/department.service.ts
+++ Frontend/src/app/department/department.service.ts
@@ -1,59 +1,33 @@
 import { Injectable } from '@angular/core';
 import { map } from "rxjs/operators";
-import { HttpClient } from '@angular/common/http';
-import { AuthService } from '../shared/auth.service';
 import { Department } from './department.model';
+import { HttpService } from '../shared/http.service';
 
 @Injectable({providedIn: 'root'})
 export class DepartmentService {
 
   constructor(
-    private http: HttpClient,
-    private auth: AuthService
+    private httpServ: HttpService
   ) {}
 
   fetchDepartmentByName(name) {
-      return this.http.get(
-        this.auth.apiUrl + '/department/' + name + "/"
-      )
+    return this.httpServ.fetchByValue("department", name)
       .pipe(
         map((responseData: Department) => {
-        return responseData;
+          return responseData;
         })
       )
   }
   //Gets specific department by name
 
   fetchDepartments() {
-      return this.http.get(
-        this.auth.apiUrl + '/department'
-      )
+    return this.httpServ.fetchAll("department")
       .pipe(
         map((responseData: Department[]) => {
-            return responseData;
+          return responseData;
         })
       )
   }
-  //Gets departments of specific type (lathe/mill) based on current param value, 
-  // or input value for tutorial checks
-
-  addDepartment(data: Department){
-      return this.http.post(
-        this.auth.apiUrl + '/department/', data
-      );
-  }
-  //Posts new department to API
-
-  updateDepartment(data, name){
-      return this.http.put(
-        this.auth.apiUrl + '/department/' + name, data
-      );
-  }
-  //Updates currentJob and currentOp valued for department
-
-  deleteDepartment(name){
-    return this.http.delete(this.auth.apiUrl + "/department/" + name + "/")
-  }
-  //Deletes selected department
+  //Gets all departments
 
 }
---
+++ Frontend/src/app/shared/http.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../shared/auth.service';
+
+@Injectable({providedIn: 'root'})
+export class HttpService {
+
+  constructor(
+    private http: HttpClient,
+    private auth: AuthService
+  ) {}
+
+  fetchByValue(model, value) {
+    return this.http.get(
+        this.auth.apiUrl + '/' + model + '/' + value + "/"
+    )
+  }
+  //Gets specific model by defining value(s)
+
+  fetchAll(model) {
+    return this.http.get(
+        this.auth.apiUrl + '/' + model 
+    );
+  }
+  //Gets all of an item for user
+
+  addItem(model, data){
+    return this.http.post(
+        this.auth.apiUrl + '/' + model , data
+    );
+  }
+  //Posts new item to API
+
+  updateItem(model, data, value){
+    return this.http.put(
+        this.auth.apiUrl  + '/' + model + '/' + value, data
+    );
+  }
+  //Updates selected item
+
+  deleteItem(model, name){
+    return this.http.delete(
+        this.auth.apiUrl  + '/' + model + '/' + name
+    );
+  }
+  //Deletes selected item
+
+}

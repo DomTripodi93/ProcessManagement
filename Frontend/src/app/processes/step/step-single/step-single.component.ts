f41371c933511f2c93c7bf66b5a2d7d265276a97
DomTripodi93 <dtripodi93@gmail.com>
2020-02-22 06:15:47 -0500

    single step display for step component

--- Frontend/src/app/processes/step/step-single/step-single.component.ts
+++ Frontend/src/app/processes/step/step-single/step-single.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { Step } from '../step.model';
+import { Subscription } from 'rxjs';
+import { StepService } from '../step.service';
+import { HelperService } from 'src/app/shared/helper.service';
 
 @Component({
   selector: 'app-step-single',
   templateUrl: './step-single.component.html',
   styleUrls: ['./step-single.component.css']
 })
-export class StepSingleComponent implements OnInit {
+export class StepSingleComponent implements OnInit, OnDestroy {
+  @Input() step: Step;
+  @Input() inDepartment: boolean;
+  subscriptions: Subscription[] = [];
+  editMode: boolean = false;
 
-  constructor() { }
+  constructor(
+    private stepServ: StepService,
+    private helpers: HelperService
+  ) { }
 
   ngOnInit() {
+    this.step.stepNumber = this.helpers.dashToSlash(this.step.stepNumber);
+    this.subscriptions.push(this.stepServ.stepCancel.subscribe(()=>{
+      this.editMode = false;
+    }));
   }
 
+  editStep(){
+    this.editMode = true;
+  }
+  //Shows edit form for selected Step
+
+  onDelete(){
+    if (confirm(
+      "Are you sure you want to delete the " +this.step.stepNumber+ " step?"
+      )){
+      this.stepServ.deleteStep(
+        this.step.deptName, this.step.objectiveName, this.step.stepNumber
+      ).subscribe(()=>{
+        this.stepServ.stepChanged.next();
+      });
+    }
+  }
+  //Deletes selected Step
+
+  ngOnDestroy(){
+    this.subscriptions.forEach(sub =>{
+      sub.unsubscribe();
+    })
+  }
+  //Unsubscribes from all component subscriptions
 }

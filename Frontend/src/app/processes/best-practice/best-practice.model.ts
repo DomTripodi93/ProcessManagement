1622151b7247b877561e8601c5a078394e20c66e
DomTripodi93 <dtripodi93@gmail.com>
2020-02-23 03:32:42 -0500

    added best practice model

---
+++ Frontend/src/app/processes/best-practice/best-practice.model.ts
@@ -0,0 +1,9 @@
+export interface BestPractice{
+    id?: number,
+    objectiveName: string,
+    deptName: string,
+    stepNumber: string,
+    practice: string,
+    method: string,
+    purpose: string
+}

89d30879a1e7d88234071b01afd599e6fce7f8a5
DomTripodi93 <dtripodi93@gmail.com>
2020-02-23 03:26:45 -0500

    added common difficulty model

---
+++ Frontend/src/app/processes/common-difficulty/common-difficulty.model.ts
@@ -0,0 +1,9 @@
+export interface CommonDifficulty{
+    id?: number,
+    objectiveName: string,
+    deptName: string,
+    stepNumber: string,
+    difficulty: string,
+    cause: string,
+    solution: string
+}

78dec47c8b927fe8d1e2d9ce2afa95c694be7fb8
DomTripodi93 <dtripodi93@gmail.com>
2020-02-23 03:35:28 -0500

    added best practice form component code

--- Frontend/src/app/processes/best-practice/best-practice-form/best-practice-form.component.ts
+++ Frontend/src/app/processes/best-practice/best-practice-form/best-practice-form.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HelperService } from 'src/app/shared/helper.service';
+import { AuthService } from 'src/app/shared/auth.service';
+import { BestPractice } from '../best-practice.model';
+import { BestPracticeService } from '../best-practice.service';
 
 @Component({
   selector: 'app-best-practice-form',
@@ -6,10 +11,93 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./best-practice-form.component.css']
 })
 export class BestPracticeFormComponent implements OnInit {
+  @Input() bestPracticeDefault: BestPractice;
+  @Input() department: string;
+  @Input() objective: string;
+  @Input() step: string;
+  canInput= false;
+  bestPracticeForm: FormGroup;
+  isError = false;
+  error = "";
+  
+  constructor(
+    private bestPracticeServ: BestPracticeService,
+    private helpers: HelperService,
+    public auth: AuthService
+  ){}
+  
+  ngOnInit(){
+    this.canInput = this.auth.isAuthenticated;
+    this.pickForm();
+  }
 
-  constructor() { }
+  pickForm(){
+    if (this.bestPracticeDefault){
+      this.initForm(this.bestPracticeDefault)
+    } else {
+      let emptyBestPractice: BestPractice = {
+        practice: "",
+        stepNumber: this.step,
+        deptName: this.department,
+        objectiveName: this.objective,
+        method: "",
+        purpose: ""
+      };
+      this.initForm(emptyBestPractice);
+    }
+  }
+    
+  private initForm(formBestPractice: BestPractice) {
+    this.bestPracticeForm = new FormGroup({
+      'stepNumber': new FormControl(formBestPractice.stepNumber, Validators.required),
+      'deptName': new FormControl(formBestPractice.deptName, Validators.required),
+      'objectiveName': new FormControl(formBestPractice.objectiveName, Validators.required),
+      'practice': new FormControl(formBestPractice.practice, Validators.required),
+      'method': new FormControl(formBestPractice.method),
+      'purpose': new FormControl(formBestPractice.purpose, Validators.required),
+    });
+  }
 
-  ngOnInit() {
+
+  async onSubmit(){
+    if (this.bestPracticeDefault){
+      this.bestPracticeForm.value.practice = await this.prepBestPracticeName(this.bestPracticeForm.value.practice);
+      this.updateBestPractice(this.bestPracticeForm.value);
+    } else {
+      this.bestPracticeForm.value.practice = await this.prepBestPracticeName(this.bestPracticeForm.value.practice);
+      this.newBestPractice(this.bestPracticeForm.value);
+    }
+  }
+
+  async prepBestPracticeName(practice: string){
+    practice = this.helpers.capitalize(practice);
+    practice = this.helpers.slashToDash(practice);
+    practice = await this.helpers.removeSpaceAtEnd(practice);
+
+    return practice;
+  }
+
+  updateBestPractice(data: any) {
+    this.bestPracticeServ.updateBestPractice(
+      data, 
+      this.bestPracticeDefault.id
+    ).subscribe(()=>{
+      this.bestPracticeServ.bestPracticeChanged.next();
+    });
+  }
+
+  newBestPractice(data: BestPractice) {
+    this.bestPracticeServ.addBestPractice(data).subscribe(()=>{
+      this.bestPracticeServ.bestPracticeChanged.next();
+    },
+    () =>{
+      this.isError = true;
+      this.error = "That best practice already exists for this objective";
+    });
+  }
+
+  onCancel(){
+    this.bestPracticeServ.bestPracticeCancel.next();
   }
 
 }

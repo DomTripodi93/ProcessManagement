c2e83de75f4a4d4b7e6e9b0901d84ff3870c2175
DomTripodi93 <dtripodi93@gmail.com>
2020-02-23 03:28:18 -0500

    added common difficulty single component code

--- Frontend/src/app/processes/common-difficulty/common-difficulty-single/common-difficulty-single.component.ts
+++ Frontend/src/app/processes/common-difficulty/common-difficulty-single/common-difficulty-single.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { HelperService } from 'src/app/shared/helper.service';
+import { Subscription } from 'rxjs';
+import { CommonDifficulty } from '../common-difficulty.model';
+import { CommonDifficultyService } from '../common-difficulty.service';
 
 @Component({
   selector: 'app-common-difficulty-single',
   templateUrl: './common-difficulty-single.component.html',
   styleUrls: ['./common-difficulty-single.component.css']
 })
-export class CommonDifficultySingleComponent implements OnInit {
+export class CommonDifficultySingleComponent implements OnInit, OnDestroy {
+  @Input() commonDifficulty: CommonDifficulty;
+  @Input() inStep: boolean;
+  subscriptions: Subscription[] = [];
+  editMode: boolean = false;
 
-  constructor() { }
+  constructor(
+    private commonDifficultyServ: CommonDifficultyService,
+    private helpers: HelperService
+  ) { }
 
   ngOnInit() {
+    this.commonDifficulty.difficulty = this.helpers.dashToSlash(this.commonDifficulty.difficulty);
+    this.subscriptions.push(this.commonDifficultyServ.commonDifficultyCancel.subscribe(()=>{
+      this.editMode = false;
+    }));
   }
 
+  editCommonDifficulty(){
+    this.editMode = true;
+  }
+  //Shows edit form for selected CommonDifficulty
+
+  onDelete(){
+    if (confirm(
+      "Are you sure you want to delete the " +this.commonDifficulty.difficulty+ " commonDifficulty?"
+      )){
+      this.commonDifficultyServ.deleteCommonDifficulty(this.commonDifficulty.id).subscribe(()=>{
+        this.commonDifficultyServ.commonDifficultyChanged.next();
+      });
+    }
+  }
+  //Deletes selected CommonDifficulty
+
+  ngOnDestroy(){
+    this.subscriptions.forEach(sub =>{
+      sub.unsubscribe();
+    })
+  }
+  //Unsubscribes from all component subscriptions
 }

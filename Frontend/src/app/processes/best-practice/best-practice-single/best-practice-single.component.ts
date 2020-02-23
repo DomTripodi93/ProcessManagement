442f6de9ce1e46849bc41cbd9c0a1e18cac7ebb8
DomTripodi93 <dtripodi93@gmail.com>
2020-02-23 03:34:58 -0500

    added best practice single component code

--- Frontend/src/app/processes/best-practice/best-practice-single/best-practice-single.component.ts
+++ Frontend/src/app/processes/best-practice/best-practice-single/best-practice-single.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { HelperService } from 'src/app/shared/helper.service';
+import { BestPractice } from '../best-practice.model';
+import { BestPracticeService } from '../best-practice.service';
 
 @Component({
   selector: 'app-best-practice-single',
   templateUrl: './best-practice-single.component.html',
   styleUrls: ['./best-practice-single.component.css']
 })
-export class BestPracticeSingleComponent implements OnInit {
+export class BestPracticeSingleComponent implements OnInit, OnDestroy {
+  @Input() bestPractice: BestPractice;
+  @Input() inStep: boolean;
+  subscriptions: Subscription[] = [];
+  editMode: boolean = false;
 
-  constructor() { }
+  constructor(
+    private bestPracticeServ: BestPracticeService,
+    private helpers: HelperService
+  ) { }
 
   ngOnInit() {
+    this.bestPractice.practice = this.helpers.dashToSlash(this.bestPractice.practice);
+    this.subscriptions.push(this.bestPracticeServ.bestPracticeCancel.subscribe(()=>{
+      this.editMode = false;
+    }));
   }
 
+  editBestPractice(){
+    this.editMode = true;
+  }
+  //Shows edit form for selected BestPractice
+
+  onDelete(){
+    if (confirm(
+      "Are you sure you want to delete the " +this.bestPractice.practice+ " bestPractice?"
+      )){
+      this.bestPracticeServ.deleteBestPractice(this.bestPractice.id).subscribe(()=>{
+        this.bestPracticeServ.bestPracticeChanged.next();
+      });
+    }
+  }
+  //Deletes selected BestPractice
+
+  ngOnDestroy(){
+    this.subscriptions.forEach(sub =>{
+      sub.unsubscribe();
+    })
+  }
+  //Unsubscribes from all component subscriptions
 }

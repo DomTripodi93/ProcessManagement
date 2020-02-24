76cb060b2ad7bc16a91cca3f2f0e52b69a8c568d
DomTripodi93 <dtripodi93@gmail.com>
2020-02-24 10:19:24 -0500

    added backend employeeIdIncrement model

---
+++ Backend/Models/EmployeeIdIncrement.cs
@@ -0,0 +1,9 @@
+namespace Backend.Models
+{
+    public class EmployeeIdIncrement
+    {
+        public User User { get; set; }
+        public int userId { get; set; }
+        public int employeeId { get; set; }
+    }
+}

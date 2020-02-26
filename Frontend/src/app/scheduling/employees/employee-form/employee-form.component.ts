87a71f1c2e247ace8df74fd1fc50cd1004ccf91f
DomTripodi93 <dtripodi93@gmail.com>
2020-02-26 12:44:43 -0500

    added employee form code

--- Frontend/src/app/scheduling/employees/employee-form/employee-form.component.ts
+++ Frontend/src/app/scheduling/employees/employee-form/employee-form.component.ts
@@ -1,15 +1,109 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { Employee } from '../employee.model';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { EmployeeService } from '../employee.service';
+import { AuthService } from 'src/app/shared/auth.service';
+import { DepartmentService } from '../../../processes/department/department.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-employee-form',
   templateUrl: './employee-form.component.html',
   styleUrls: ['./employee-form.component.css']
 })
-export class EmployeeFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class EmployeeFormComponent implements OnInit, OnDestroy {
+  @Input() employeeDefault: Employee;
+  subscriptions: Subscription[] = [];
+  departments: string[] = [""];
+  canEdit: boolean[]= [true, false]
+  canInput= false;
+  employeeForm: FormGroup;
+  isError = false;
+  error = "";
+  
+  constructor(
+    private employeeServ: EmployeeService,
+    private deptServ: DepartmentService,
+    public auth: AuthService
+  ){}
+  
+  ngOnInit(){
+    this.canInput = this.auth.isAuthenticated;
+    this.getDepartments();
+    this.pickForm();
   }
+
+  getDepartments(){
+    this.subscriptions.push(
+      this.deptServ.fetchDepartments().subscribe(departments =>{
+        departments.forEach(department =>{
+          this.departments.push(department.deptName);
+        })
+      })
+    );
+  }
+  //Gets all departments for user
+
+  pickForm(){
+    if (this.employeeDefault){
+      this.initForm(this.employeeDefault)
+    } else {
+      let emptyEmployee: Employee = {
+        name: "",
+        deptName: "",
+        title:"",
+        canEdit: false
+      };
+      this.initForm(emptyEmployee);
+    }
+  }
+    
+  private initForm(formEmployee: Employee) {
+    this.employeeForm = new FormGroup({
+      'name': new FormControl(formEmployee.name, Validators.required),
+      'deptName': new FormControl(formEmployee.deptName),
+      'title': new FormControl(formEmployee.name, Validators.required),
+      'canEdit': new FormControl(formEmployee.canEdit, Validators.required),
+    });
+  }
+
+
+  async onSubmit(){
+    if (this.employeeDefault){
+      this.updateEmployee(this.employeeForm.value);
+    } else {
+      this.newEmployee(this.employeeForm.value);
+    }
+  }
+
+  updateEmployee(data: any) {
+    this.employeeServ.updateEmployee(
+      data, 
+      this.employeeDefault.employeeId
+    ).subscribe(()=>{
+      this.employeeServ.employeeChanged.next();
+    });
+  }
+
+  newEmployee(data: Employee) {
+    this.employeeServ.addEmployee(data).subscribe(()=>{
+      this.employeeServ.employeeChanged.next();
+    },
+    () =>{
+      this.isError = true;
+      this.error = "That employee already exists";
+    });
+  }
+
+  onCancel(){
+    this.employeeServ.employeeCancel.next();
+  }
+
+  ngOnDestroy(){
+    this.subscriptions.forEach(sub =>{
+      sub.unsubscribe();
+    })
+  }
+  //Unsubscribes from all component subscriptions
 
 }

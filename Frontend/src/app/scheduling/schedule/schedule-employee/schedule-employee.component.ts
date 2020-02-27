d5b676a0e1d563103337c52c3a56e6fee811190a
DomTripodi93 <dtripodi93@gmail.com>
2020-02-27 12:08:15 -0500

    added basic calender with styling for schedule component

--- Frontend/src/app/scheduling/schedule/schedule-employee/schedule-employee.component.ts
+++ Frontend/src/app/scheduling/schedule/schedule-employee/schedule-employee.component.ts
@@ -9,7 +9,7 @@ export class ScheduleEmployeeComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
--- Frontend/src/app/scheduling/schedule/schedule.component.ts
+++ Frontend/src/app/scheduling/schedule/schedule.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import _ from 'lodash';
 
 @Component({
   selector: 'app-schedule',
@@ -6,10 +8,72 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./schedule.component.css']
 })
 export class ScheduleComponent implements OnInit {
+  @ViewChild('newMonth') newMonthForm: NgForm;
+  date = new Date();
+  today = this.date.getDate();
+  month = this.date.getMonth();
+  monthHold = ""+(this.month+1);
+  year = this.date.getFullYear();
+  day = this.date.getDay();
+  defaultMonth = ""; 
+  oldMonth: number = this.month;
+  days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ]
+  months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+  numberOfDays: number;
+  monthDays = []
+  firstDayOfMonth = []
+  firstDay: Date;
 
   constructor() { }
 
   ngOnInit() {
+    this.initializeMonth();
   }
 
+  initializeMonth(){
+    if (this.month < 10){
+      this.monthHold ="0"+this.monthHold;
+    }
+    this.defaultMonth = this.year +"-"+ this.monthHold;
+    this.setDate();
+  }
+
+  setDate(){
+    this.numberOfDays = this.daysInMonth(this.year, this.month+1);
+    this.monthDays = _.range(1, this.numberOfDays + 1);
+    this.firstDay = new Date(this.year, this.month, 1);
+    this.firstDayOfMonth = _.range(0, this.firstDay.getDay());
+  }
+
+  daysInMonth(year: number, month: number){
+    return new Date(year, month, 0).getDate();
+  }
+
+  changeDate(){
+    let hold = this.newMonthForm.value.date.split("-")
+    this.year = +hold[0];
+    this.month = +hold[1] - 1;
+    this.setDate();
+  }
 }
